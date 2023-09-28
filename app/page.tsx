f268b689d7447cdc1df53ff35fe8a661165ab0c7
madhavdutta <madhavdutta@gmail.com>
2023-09-28 17:51:41 +0530

    Some text added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Button } from '@mantine/core'
+import { Button, Group } from '@mantine/core'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+     <Group>
+        Hello world!
+      </Group>
     </main>
   )
 }
